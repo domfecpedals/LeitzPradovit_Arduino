b2c2793dad3c4fdd4f20a59e6e9e34726640499a
Yuan <domfecpedals@gmail.com>
2014-03-17 21:27:51 +0800

    Advance button modified
    need to connect this pin to ground if not connect to high, otherwise
    its value will be random between 0 and 1.

--- LeitzPradovit_Arduino.ino
+++ LeitzPradovit_Arduino.ino
@@ -7,8 +7,8 @@ LiquidCrystal_I2C lcd(0x27,16,2);
 
 int left=12;
 int right=13;
-int advanceDelay=14;
-int highRef=15;
+int advanceDelay=4;
+int highRef=5;
 
 void setup() {
   Serial.begin(4800);
@@ -24,75 +24,88 @@ void setup() {
   lcd.print("Leitz Pradovit"); // Print a message to the LCD.
   lcd.setCursor(0,1); //newline
   lcd.print("Initialized");// Print a message to the LCD
-  digitalWrite(highRef,HIGH);
+  
 }
 
 void loop() {
+  digitalWrite(highRef,HIGH);
   char c;
   char cmd;
   char l='1';
   char r='2';
+  int state=0;
+  state=digitalRead(advanceDelay);
+
   
-  if (advanceDelay==HIGH){
-   delay(1000); 
+  
+  if (altSerial.available()) {
+      c = altSerial.read();
+      lcd.setCursor(0,1); //newline
+      lcd.print("Focusing!");
+      Serial.print(c);
+    
+    
+     if (state==1){
+   digitalWrite(left,LOW);
+   digitalWrite(right,LOW);
+   delay(2000); 
+   digitalWrite(left,HIGH);
+   digitalWrite(right,LOW);
+   delay(500);
   }
   
-  if (altSerial.available()){
-    cmd=altSerial.read();
-          lcd.setCursor(0,1); //newline
-      lcd.print("Focusing!");
-    switch (cmd){
-    case '1':
-    
+    if (c==l){
+
       Serial.print("left");
       digitalWrite(left,HIGH);
       digitalWrite(right,LOW);
-    case '2':
+//      lcd.setCursor(0,0); //newline
+//      lcd.print("Motor Left");
+    }
+    else if(c==r)
+    {
+
       Serial.print("right");
       digitalWrite(left,LOW);
       digitalWrite(right,HIGH);
-    
-    default:
-    
+//      lcd.setCursor(0,1); //newline
+//      lcd.print("Motor Right");
+    }
+    else 
+    {
+
       Serial.print("stop");
       digitalWrite(left,LOW);
       digitalWrite(right,LOW);
-      lcd.setCursor(0,1);
-  }
-  }
-}
+      lcd.setCursor(0,1); //newline
+//      lcd.print("Focused");
+    }
   
-//  if (altSerial.available()) {
-//    c = altSerial.read();
+//  if (altSerial.available()){
+//    cmd=altSerial.read();
 //      lcd.setCursor(0,1); //newline
 //      lcd.print("Focusing!");
+//    switch (cmd){
+//    case '1':
 //    
-//    Serial.print(c);
-//    if (c==l){
-//
 //      Serial.print("left");
 //      digitalWrite(left,HIGH);
 //      digitalWrite(right,LOW);
-////      lcd.setCursor(0,0); //newline
-////      lcd.print("Motor Left");
-//    }
-//    else if(c==r)
-//    {
-//
+//    case '2':
 //      Serial.print("right");
 //      digitalWrite(left,LOW);
 //      digitalWrite(right,HIGH);
-////      lcd.setCursor(0,1); //newline
-////      lcd.print("Motor Right");
-//    }
-//    else 
-//    {
-//
+//    
+//    default:
+//    
 //      Serial.print("stop");
 //      digitalWrite(left,LOW);
 //      digitalWrite(right,LOW);
-//      lcd.setCursor(0,1); //newline
-////      lcd.print("Focused");
-//    }
+//      lcd.setCursor(0,1);
+//  }
+  }
+}
+  
+
 //  }
 
